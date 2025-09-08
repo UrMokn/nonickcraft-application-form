fed0e4229fcd3917304235a2569332b679439ff8
UrMokn <urmoknmc@gmail.com>
2025-09-08 16:49:12 +0900

    Update layout.tsx

--- src/app/(application-form)/layout.tsx
+++ src/app/(application-form)/layout.tsx
@@ -10,7 +10,7 @@ export default function Layout({ children }: { children: ReactNode }) {
       <div className='container max-w-screen-md flex flex-col gap-6'>
         <Image src={banner} alt='バナー画像' className='rounded-lg' priority />
         <section className='space-y-1'>
-          <h1 className='max-sm:text-2xl text-3xl font-black'>のにクラ参加申請フォーム</h1>
+          <h1 className='max-sm:text-2xl text-3xl font-black'>UrMoknCraft参加申請フォーム</h1>
           <h2 className='max-sm:text-sm text-default-500'>
             UrMoknCraftへの参加申請を行えます。
           </h2>
